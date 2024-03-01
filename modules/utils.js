b05855de1e234096c59742ba2b024c9cbbd2fac5
SeoSeunghun <proto_seo@naver.com>
2024-03-01 23:31:09 +0900

    feat: add base64 decode function

--- modules/utils.js
+++ modules/utils.js
@@ -19,3 +19,9 @@ export function stringToBase64(str) {
   const binString = String.fromCodePoint(...bytes);
   return btoa(binString);
 }
+
+export function base64ToString(base64) {
+  const binString = atob(base64);
+  const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0));
+  return new TextDecoder().decode(bytes);
+}
