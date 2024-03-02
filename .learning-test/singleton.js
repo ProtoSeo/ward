ecbd2aa496a1b433626d97670d6e14ba6c338374
SeoSeunghun <proto_seo@naver.com>
2024-03-02 19:48:11 +0900

    test: add singleton learning

--- .learning-test/singleton.js
+++ .learning-test/singleton.js
@@ -1,16 +1,24 @@
 class Singleton {
 
-  constructor() {
+  constructor(height, width) {
     if (Singleton.instance) {
       return Singleton.instance;
     }
+    this.height = height;
+    this.width = width;
     Singleton.instance = this;
+  }
+
+  getArea() {
+    return this.height * this.width;
   }
 }
 
 function isSame() {
-  const a = new Singleton();
-  const b = new Singleton();
+  const a = new Singleton(10, 11);
+  const b = new Singleton(5, 1);
+  console.log(a.getArea() === 110);
+  console.log(b.getArea() === 110);
   console.log(a === b);
 }
 
